a0b1f8dea2d592f678b74a5efc243c32fe81a918
kuroharu0430 <kuroharu0430@gmail.com>
2019-08-29 12:38:53 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "13.113.167.56", user: "deploy", roles: %w{app db web}
+server "13.113.167.56", user: "ec2-user", roles: %w{app db web}
 
 
 
